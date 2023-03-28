acacf70eb607d5cf04ddc2e053ce7df1379e8b9b
yousef775 <yhousein@gmail.com>
2023-03-28 15:45:54 +0200

    Home_appbar

--- lib/screens/main_screens/home_screen.dart
+++ lib/screens/main_screens/home_screen.dart
@@ -1,13 +1,42 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../../widgets/input_widgets/text_field_widget.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchController = TextEditingController();
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(),
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context)!.ojali,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+        ),
+        leading: const Icon(Icons.shopping_cart),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFieldWidget(
+                  controller: searchController,
+                  hintText: AppLocalizations.of(context)!.search,
+                  obSecureText: false,
+                  perfix: const Icon(Icons.search),
+                  validator: (String? value) {}),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
